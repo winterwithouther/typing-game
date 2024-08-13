ab582f55c0ca38403c57667ced106e9078dd487c
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-08-13 17:16:52 -0400

    formatted words so it shows 3 lines only

--- typing.js
+++ typing.js
@@ -1,17 +1,23 @@
-const words = "but or and where how some cool people however something then when done they him her state".split(" ")
-const wordsCount = words.length
+const words = "but or and where how some cool people however something then when done they him her state".split(" ");
+const wordsCount = words.length;
 
 function randomWord() {
-    const randomIndex = Math.ceil(Math.random() * wordsCount)
-    return words[randomIndex]
+    const randomIndex = Math.floor(Math.random() * wordsCount);
+    return words[randomIndex];
+}
+
+function formatWord(word) {
+    return `<div class="word">${word}</div>`;
 }
 
 function newGame() {
-    wordDiv = document.getElementById("words")
-    wordDiv.innerHTML = ""
+    const wordDiv = document.getElementById("words");
+    wordDiv.innerHTML = "";
     for (let i = 0; i < 200; i++) {
-        randomWordRetrieved = randomWord()
-        wordDiv.innerHTML += randomWordRetrieved
+        const randomWordRetrieved = randomWord();
+        const formattedRandomWord = formatWord(randomWordRetrieved);
+        wordDiv.innerHTML += formattedRandomWord;
     }
 }
-newGame()
+
+newGame();
