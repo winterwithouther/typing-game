5a5ccdf64ec177973f365c3828062992010afd15
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-08-13 18:26:43 -0400

    auto incorrecting all leftover letters after hitting space on current word and moving on to the next word as current

--- typing.js
+++ typing.js
@@ -10,15 +10,13 @@ function removeClass(el, name) {
 }
 
 function randomWord() {
-    const randomIndex = Math.floor(Math.random() * wordsCount);
-    return words[randomIndex];
+    const randomIndex = Math.ceil(Math.random() * wordsCount);
+    return words[randomIndex - 1];
 }
 
 function formatWord(word) {
     return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
 }
-
-
 
 function newGame() {
     const wordDiv = document.getElementById("words");
@@ -34,9 +32,39 @@ function newGame() {
 
 document.getElementById("game").addEventListener("keyup", e => {
     const key = e.key
+    const currentWord = document.querySelector(".word.current")
     const currentLetter = document.querySelector('.letter.current')
     const expected = currentLetter.innerHTML
-    console.log({key, expected})
+
+    const isLetter = key.length === 1 && key !== " ";
+    const isSpace = key === " "
+
+    if (isLetter) {
+        if (currentLetter) {
+            addClass(currentLetter, key === expected ? "correct" : "incorrect")
+            removeClass(currentLetter, "current")
+            addClass(currentLetter.nextSibling, "current")
+        }
+    }
+    
+    if (isSpace) {
+        if (expected !== ' ') {
+            const lettersToInvalidate = [...document.querySelectorAll(".word.current .letter:not(.correct)")]
+            lettersToInvalidate.forEach(letter => {
+                addClass(letter, "incorrect")
+            })
+        }
+        removeClass(currentWord, "current");
+        addClass(currentWord.nextSibling, "current");
+        if (currentLetter) {
+            removeClass(currentLetter, "current")
+        }
+
+        addClass(currentWord.nextSibling.firstChild, "current") 
+    }
+
+    
 })
+
 
 newGame();
